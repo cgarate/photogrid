d4cc97e3873d83551bc33a6d0bc692ade78bf831
Carlos Garate <cgarate@carlostwlaptop>
2021-01-10 17:10:59 -0500

    create a Footer component and set basic structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import {
+  FooterContainer,
+  FlexContainer,
+  AvatarContainer,
+} from "./footerStyles";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FlexContainer>
+        <AvatarContainer></AvatarContainer>
+      </FlexContainer>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
