e5873718c28cb63df85dff4f94a3b9656f2ac25f
Carlos Garate <cgarate@carlostwlaptop>
2021-01-10 17:10:34 -0500

    add footer styled components

---
+++ src/components/footerStyles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  height: 75px;
+  width: 100%;
+  background-color: #d4e5f7;
+  position: fixed;
+  color: #ffffff;
+  left: 0;
+  bottom: 0;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const AvatarContainer = styled.div``;
