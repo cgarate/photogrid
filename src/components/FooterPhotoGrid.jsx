2cd58f1ee51750ffb27a694b9bd24a18ba79b7e3
Carlos Garate <cgarate@carlostwlaptop>
2021-01-11 01:32:06 -0500

    refactor footer with components to display data

--- src/components/FooterPhotoGrid.jsx
+++ src/components/FooterPhotoGrid.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { BsPersonBoundingBox } from "react-icons/bs";
+import PropTypes from "prop-types";
+import { BiShareAlt } from "react-icons/bi";
 
 import {
   FooterContainer,
   FlexContainer,
-  AvatarContainer,
-  FooterMessages,
+  Container,
 } from "./footerPhotoGridStyles";
+import ShowSelected from "./ShowSelected";
 
-const FooterPhotoGrid = () => {
+const FooterPhotoGrid = ({ countSelected, clearSelection }) => {
   return (
     <FooterContainer>
       <FlexContainer>
-        <FooterMessages></FooterMessages>
-        <AvatarContainer>
+        <ShowSelected
+          countSelected={countSelected}
+          clearSelection={clearSelection}
+        />
+
+        <Container>
+          <BiShareAlt
+            style={{ marginRight: "10px" }}
+            font-size="2rem"
+            color="#999999"
+          />
           <BsPersonBoundingBox font-size="2rem" color="#999999" />
-        </AvatarContainer>
+        </Container>
       </FlexContainer>
     </FooterContainer>
   );
+};
+
+FooterPhotoGrid.propTypes = {
+  countSelected: PropTypes.number,
+  clearSelection: PropTypes.func,
 };
 
 export default FooterPhotoGrid;
