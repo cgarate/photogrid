8857cc1bf1a4c1a17393433514f3b72d65868e37
Carlos Garate <cgarate@carlostwlaptop>
2021-01-11 01:28:31 -0500

    move selected logic to its own component

---
+++ src/components/ShowSelected.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BsFillXSquareFill } from "react-icons/bs";
+
+import { FooterSelectedCount } from "./footerPhotoGridStyles";
+
+const ShowSelected = ({ countSelected, clearSelection }) => {
+  return (
+    <FooterSelectedCount>
+      {countSelected > 0 ? (
+        <BsFillXSquareFill
+          style={{ marginRight: "10px" }}
+          font-size="1.5rem"
+          color="#ff7700"
+          onClick={clearSelection}
+        />
+      ) : null}
+      Selected: {countSelected}
+    </FooterSelectedCount>
+  );
+};
+
+ShowSelected.propTypes = {
+  countSelected: PropTypes.number,
+  clearSelection: PropTypes.func,
+};
+
+export default ShowSelected;
