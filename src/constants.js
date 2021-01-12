6abbf895c0e528f53a33d7373d734a01c26141b2
Carlos Garate <carlos.m.garate@gmail.com>
2021-01-12 03:11:13 -0500

    update url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const PHOTO_API = "http://localhost:3001/photos";
-export const UPLOAD_PHOTO_API = "http://localhost:3001/website/photos/{photo-id}";
+export const UPLOAD_PHOTO_API = "http://localhost:3001/website/photos/";
 export const WS_CONNECTION = "http://localhost:3001/ws";
 
